1105d676655ba9f67a424e1087796228fbb88732
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-06 18:41:06 -0600

    add Home page to display interview cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
-const Page = () => {
+import {Button} from "@/components/ui/button";
+import InterviewCard from "@/components/InterviewCard";
+import {dummyInterviews} from "@/constants";
+
+async function Home() {
+    const user = null
+    const hasPastInterviews = true
+    const hasUpcomingInterviews = true
+
     return (
-        <div>Home Page</div>
-    )
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+                    <p className="text-lg">
+                        Practice real interview questions & get instant feedback
+                    </p>
+
+                    <Button asChild className="btn-primary max-sm:w-full">
+                        <Link href="/interview">Start an Interview</Link>
+                    </Button>
+                </div>
+
+                <Image
+                    src="/robot.png"
+                    alt="robo-dude"
+                    width={400}
+                    height={400}
+                    className="max-sm:hidden"
+                />
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
+
+                <div className="interviews-section">
+                    {hasPastInterviews ? (
+                        dummyInterviews?.map((interview) => (
+                            <InterviewCard
+                                key={interview.id}
+                                userId={user?.id}
+                                interviewId={interview.id}
+                                role={interview.role}
+                                type={interview.type}
+                                techstack={interview.techstack}
+                                createdAt={interview.createdAt}
+                            />
+                        ))
+                    ) : (
+                        <p>You haven&apos;t taken any interviews yet</p>
+                    )}
+                </div>
+            </section>
+
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take Interviews</h2>
+
+                <div className="interviews-section">
+                    {hasUpcomingInterviews ? (
+                        dummyInterviews?.map((interview) => (
+                            <InterviewCard
+                                key={interview.id}
+                                userId={user?.id}
+                                interviewId={interview.id}
+                                role={interview.role}
+                                type={interview.type}
+                                techstack={interview.techstack}
+                                createdAt={interview.createdAt}
+                            />
+                        ))
+                    ) : (
+                        <p>There are no interviews available</p>
+                    )}
+                </div>
+            </section>
+        </>
+    );
 }
-export default Page
+
+export default Home;
