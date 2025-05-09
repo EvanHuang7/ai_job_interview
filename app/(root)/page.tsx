fc0be6309a65372e3add6920af22f320bfe2f295
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-08 20:38:25 -0600

    remove unneeded files and section in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,3 @@
-import Link from "next/link";
-import Image from "next/image";
-
-import {Button} from "@/components/ui/button";
 import InterviewCard from "@/components/InterviewCard";
 
 import {getCurrentUser} from "@/server/authService";
@@ -21,27 +17,6 @@ async function Home() {
 
     return (
         <>
-            <section className="card-cta">
-                <div className="flex flex-col gap-6 max-w-lg">
-                    <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
-                    <p className="text-lg">
-                        Practice real interview questions & get instant feedback
-                    </p>
-
-                    <Button asChild className="btn-primary max-sm:w-full">
-                        <Link href="/interview">Start an Interview</Link>
-                    </Button>
-                </div>
-
-                <Image
-                    src="/robot.png"
-                    alt="robo-dude"
-                    width={400}
-                    height={400}
-                    className="max-sm:hidden"
-                />
-            </section>
-
             <section className="flex flex-col gap-6 mt-8">
                 <h2>Your Interviews</h2>
 
