5a2c6c0117cc810b634c8a7d268ee28c2f0ebe24
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-11 23:26:24 -0600

    remove try catch block for layout files

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,47 +5,40 @@ import {isAuthenticated} from "@/server/authService";
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
 import {House} from "lucide-react";
-import {toast} from "sonner";
 
 const AuthLayout = async ({children}: { children: ReactNode }) => {
-    try {
-        const isUserAuthenticated = await isAuthenticated();
-        if (isUserAuthenticated) {
-            // Redirect to home page if the user is authenticated
-            redirect("/");
-        }
+    const isUserAuthenticated = await isAuthenticated();
+    if (isUserAuthenticated) {
+        // Redirect to home page if the user is authenticated
+        redirect("/");
+    }
 
-        return (
-            <>
-                <div className="flex mx-auto max-w-7xl flex-col gap-12 mt-8 mb-12 px-16 max-sm:px-4">
-                    <div className="flex flex-row items-center justify-between gap-2 mt-2">
-                        <nav>
-                            <Link href="/" className="flex items-center gap-2 text-primary-100">
-                                <House size={32}/>
-                                <h2 className="max-md:hidden">AI Job Interview</h2>
-                            </Link>
-                        </nav>
+    return (
+        <>
+            <div className="flex mx-auto max-w-7xl flex-col gap-12 mt-8 mb-12 px-16 max-sm:px-4">
+                <div className="flex flex-row items-center justify-between gap-2 mt-2">
+                    <nav>
+                        <Link href="/" className="flex items-center gap-2 text-primary-100">
+                            <House size={32}/>
+                            <h2 className="max-md:hidden">AI Job Interview</h2>
+                        </Link>
+                    </nav>
 
-                        <div className="flex flex-row items-center gap-2">
-                            <Button
-                                className="flx items-center gap-2 text-foreground hover:text-black border bg-transparent rounded-full">
-                                <Link href="/sign-in">Sign in</Link>
-                            </Button>
-                            <Button
-                                className="flx items-center gap-2 text-foreground hover:text-black border bg-transparent rounded-full">
-                                <Link href="/sign-up">Sign up</Link>
-                            </Button>
-                        </div>
+                    <div className="flex flex-row items-center gap-2">
+                        <Button
+                            className="flx items-center gap-2 text-foreground hover:text-black border bg-transparent rounded-full">
+                            <Link href="/sign-in">Sign in</Link>
+                        </Button>
+                        <Button
+                            className="flx items-center gap-2 text-foreground hover:text-black border bg-transparent rounded-full">
+                            <Link href="/sign-up">Sign up</Link>
+                        </Button>
                     </div>
                 </div>
+            </div>
 
-                <div className="flex items-center justify-center max-sm:px-4 max-sm:py-8 mb-10">{children}</div>
-            </>
-        )
-    } catch (error) {
-        console.error("Error checking user authentication:", error);
-        toast.error("There is an error when getting data");
-
-    }
+            <div className="flex items-center justify-center max-sm:px-4 max-sm:py-8 mb-10">{children}</div>
+        </>
+    )
 }
 export default AuthLayout
